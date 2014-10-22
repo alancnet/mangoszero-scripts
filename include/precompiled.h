fb9e6dc579253d6dd935e624e36b9cc96c82b9ec
Daniel S. Reichenbach <daniel@kogitoapp.com>
2014-10-22 11:04:07 +0200

    [Fix] Windows build needs this.
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

--- include/precompiled.h
+++ include/precompiled.h
@@ -39,14 +39,6 @@
 #include "sc_grid_searchers.h"
 #include "sc_instance.h"
 
-#ifdef WIN32
-#  include <windows.h>
-BOOL APIENTRY DllMain(HANDLE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
-{
-    return true;
-}
-#endif
-
 // sc_gossip.h:             ADD_GOSSIP_ITEM_EXTENDED outcommented box-money (Required until professions are fixed)
 // sc_creature.cpp:         Used in ScriptedAI::SelectSpell, outcommented SchoolMask
 
